73cb2065502c26a4e0a4532903ff753ec87179c2
MonaDXWang <mona.wang@mail.utoronto.ca>
2021-03-18 16:01:33 -0400

    Accomodated for stig2 and 3 information

--- scripts/data_organizer.py
+++ scripts/data_organizer.py
@@ -7,13 +7,31 @@ from neuron_info import ntype, nclass
 def get_neuron_pair_key (pre, post):
     return (pre, post)
 
-def merge(dict1, dict2):
-    new = {**dict1, **dict2}
+def merge(dict1, dict2, dict3, dict4):
+    new = {**dict1, **dict2, **dict3, **dict4}
     return new
 
-def append_daf_2_to_nondauers(daf2, nondauer):
+def input_dauer_json_converter(file,name):
 
-    daf2_original = open_json(daf2)
+    dauer_original = open_json(file)
+    dauer_connections = {}
+    dauer_connections[name] = {}
+
+    #Converting the daf2 json to the same format as the nondauers
+    for i, con in enumerate(dauer_original):
+
+        key = str(get_neuron_pair_key(dauer_original[i]['partners'][0], dauer_original[i]['partners'][1]))
+
+        if key not in dauer_connections.keys():
+            dauer_connections[name][key] = 1
+        
+        else:
+            dauer_connections[name][key] += 1
+    
+    return dauer_connections
+
+def rename_nondauers(nondauer):
+
     nondauer_original = open_json(nondauer)
     nondauer_renamed = {}
 
@@ -43,24 +61,13 @@ def append_daf_2_to_nondauers(daf2, nondauer):
         else:
             nondauer_renamed['adult_SEM'] = nondauer_original[dataset]
         
-    daf2_connections = {}
-    daf2_connections['daf2-dauer'] = {}
+    return nondauer_renamed
 
-    #Converting the daf2 json to the same format as the nondauers
-    for i, con in enumerate(daf2_original):
-
-        key = str(get_neuron_pair_key(daf2_original[i]['partners'][0], daf2_original[i]['partners'][1]))
-
-        if key not in daf2_connections.keys():
-            daf2_connections['daf2-dauer'][key] = 1
-        
-        else:
-            daf2_connections['daf2-dauer'][key] += 1
-    
+def append_dauer_to_nondauers(nondauer, stig2, stig3, daf2):
     #Added the daf2 dauer connections to the nondauer connections
-    connections = merge(nondauer_renamed, daf2_connections)
+    connections = merge(nondauer, daf2, stig2, stig3)
 
-    output_path = './output/transformed-connections.json'
+    output_path = 'daf2-dauer-comparisons\\output\\transformed-connections.json'
 
     with open(output_path, 'w') as f:
         json.dump(connections, f, indent= 2)
@@ -103,7 +110,7 @@ def get_class_connections(data):
     #Help check numbers with nemanode to make sure it's currectly added
     #print(class_connections['L1_1']['SAA$AVA'], class_connections['L1_2']['SAA$AVA'], class_connections['L1_3']['SAA$AVA'], class_connections['L2']['SAA$AVA'])
 
-    output_path = './output/class-connections.json'
+    output_path = 'daf2-dauer-comparisons\output\class-connections.json'
 
     with open(output_path, 'w') as f:
         json.dump(class_connections, f, indent= 2)
@@ -134,7 +141,7 @@ def get_inputs(class_json):
         for neuron in inputs[dataset]:
             inputs[dataset][neuron]['Total'] = sum(inputs[dataset][neuron].values())
     
-    output_path = './output/inputs.json'
+    output_path = 'daf2-dauer-comparisons\output\inputs.json'
 
     with open(output_path, 'w') as f:
         json.dump(inputs, f, indent= 2)
@@ -165,7 +172,7 @@ def get_outputs(class_json):
         for neuron in outputs[dataset]:
             outputs[dataset][neuron]['Total'] = sum(outputs[dataset][neuron].values())
     
-    output_path = './output/outputs.json'
+    output_path = 'daf2-dauer-comparisons\output\outputs.json'
 
     with open(output_path, 'w') as f:
         json.dump(outputs, f, indent= 2)
@@ -198,7 +205,7 @@ def get_class_connection_input_percentages(input_json, connections_list,dataset_
                 consolidated_input_percentages[connection][name] = 0.0
     
 
-    output_path = './output/input_percentages.json'
+    output_path = 'daf2-dauer-comparisons\output\input_percentages.json'
 
     with open(output_path, 'w') as f:
         json.dump(consolidated_input_percentages, f, indent= 2)
@@ -231,7 +238,7 @@ def get_class_connection_output_percentages(output_json,connections_list,dataset
             else:
                 consolidated_output_percentages[connection][name] = 0.0
     
-    output_path = './output/output_percentages.json'
+    output_path = 'daf2-dauer-comparisons\output\output_percentages.json'
 
     with open(output_path, 'w') as f:
         json.dump(consolidated_output_percentages, f, indent= 2)
@@ -242,14 +249,21 @@ def get_class_connection_output_percentages(output_json,connections_list,dataset
 
 if __name__ == '__main__':
 
-    nondauers = './input/nondauer_connections.json'
-    daf2_dauer = './input/daf2-dauer.json'
+    nondauers = 'daf2-dauer-comparisons\\input\\nondauer_connections.json'
+    daf2_dauer = 'daf2-dauer-comparisons\input\daf2-dauer.json'
+    stig2 = 'daf2-dauer-comparisons\input\stigloher2.json'
+    stig3 = 'daf2-dauer-comparisons\input\stigloher3.json'
 
-    dataset_names = ['L1_1', 'L1_2', "L1_3", 'L1_4', 'L2', 'L3', 'adult_TEM', 'adult_SEM', 'daf2-dauer']
+    dataset_names = ['L1_1', 'L1_2', "L1_3", 'L1_4', 'L2', 'L3', 'adult_TEM', 'adult_SEM', 'daf2-dauer', 'stigloher2', 'stigloher3']
     comparison_dataset_names = ['L1_1', 'L1_2', "L1_3", 'L1_4', 'L2', 'daf2-dauer']
 
 
-    nondauer_and_dauer = append_daf_2_to_nondauers(daf2_dauer, nondauers)
+    nondauer_renamed = rename_nondauers(nondauers)
+    daf2_formatted = input_dauer_json_converter(daf2_dauer, 'daf2-dauer')
+    stig2_formatted = input_dauer_json_converter(stig2, 'stigloher2')
+    stig3_formatted = input_dauer_json_converter(stig3, 'stigloher3')
+
+    nondauer_and_dauer = append_dauer_to_nondauers(nondauer_renamed, stig2_formatted, stig3_formatted, daf2_formatted)
 
     class_connections, connections_list = get_class_connections(nondauer_and_dauer)
 
--- scripts/probability_calculation.py
+++ scripts/probability_calculation.py
@@ -5,21 +5,55 @@ import csv
 import numpy as np
 
 from random import choices
-from util import open_json
+from util import open_json, write_json
 
+#filter the data for just L1 to L2 and the 3 dauer datasets
 def format_data(data, output_path):
 	dataset = open_json(data)
 	relevant_percentages = {}
 
 	for connections, dataset in dataset.items():
 		percentages = list(dataset.values())
-		relevant_percentages[connections] = percentages[0:5]
-		relevant_percentages[connections].append(percentages[-1])
+		relevant_percentages[connections] = percentages[0:6]
+		relevant_percentages[connections].extend(percentages[-3:])
 	
 	with open(output_path, 'w') as f:
 		json.dump(relevant_percentages, f, indent= 2)
 		
 	return relevant_percentages
+
+#Check whether the connections exist in the two N2 datasets
+def is_in_N2(data):
+	
+	if round(data[-2],1) != 0 and round(data[-1],1) != 0:
+		return 2
+		
+	elif (data[-2] != 0) != (data[-1] != 0):
+		return 1
+
+	else:
+		return 0
+
+#calculates the nondauer median/mean and the effect size (difference between nondauer and daf2)
+def additional_calculations(data):
+	
+	new_data = {}
+	for connection in data:
+
+		new_data[connection] = []
+
+		#Calculate and take the difference as the effect size
+
+		med = np.median(data[connection][0:6])
+		dif = np.absolute(med - data[connection][6])
+
+		new_data[connection] = data[connection][0:7]
+		new_data[connection]. extend([med,dif])
+		new_data[connection].append(is_in_N2(data[connection]))
+	
+
+
+	return new_data
 
 def bootstrap_sampling(data):
     
@@ -28,31 +62,32 @@ def bootstrap_sampling(data):
 	# load dataset
 	for connection in data:
 		i = 0
-		sample_mean = []
-		values = data[connection]
+		sample_median= []
+		values = data[connection][0:6]
+		daf2_value = data[connection][6]
+		
 
 		# Bootstrap Sampling
 		while i < random_sample_times:
-			random_sample = choices(values, k = 5)
-			avg = round(np.mean(random_sample),2)
+			random_sample = choices(values, k = 6)
+			med = np.median(random_sample)
 
-			sample_mean.append(avg)
+			sample_median.append(med)
 			i += 1
 		
-		if data[connection][-1] > np.mean(sample_mean):
-			pval = (sum(i > data[connection][-1] for i in sample_mean)+ 1)/ (1000 +1)
-
-
-		elif data[connection][-1] <= np.mean(sample_mean):
-			pval = (sum(i <= data[connection][-1] for i in sample_mean) + 1)/(1000 +1)
-
+		if daf2_value > np.median(sample_median):
+			pval = (sum(i > daf2_value for i in sample_median)+ 1)/ (1000 +1)
 		
+		elif daf2_value <= np.median(sample_median):
+			pval = (sum(i <= daf2_value for i in sample_median)+ 1)/ (1000 + 1)
+
 		data[connection].append(pval)
 	
 	return data
 
+
 def analysis_results_to_csv(filepath, data):
-	row = [['Pre', 'Post', 'L1_1', 'L1_2', "L1_3", 'L1_4', 'L2', 'daf2-dauer', 'p-value']]
+	row = [['Pre', 'Post', 'L1_1', 'L1_2', "L1_3", 'L1_4', 'L2', 'L3', 'daf2-dauer', 'nondauer median', 'effect size', 'in how many N2 dauers', 'p-value']]
 
 	for con in data:
 		name = con.split('$')
@@ -67,24 +102,32 @@ def analysis_results_to_csv(filepath, data):
 
 if __name__ == '__main__':
 
-	inputs = './output/input_percentages.json'
-	outputs = './output/output_percentages.json'
-	classification_path = './input/connection_classifications_npair.csv'
+	inputs = 'daf2-dauer-comparisons\output\input_percentages.json'
+	outputs = 'daf2-dauer-comparisons\output\output_percentages.json'
 
-	cleaned_inputs = './output/input_percentages_comparisons_only.json'
-	cleaned_outputs = './output/output_percentages_comparions_only.json'
 
-	input_changes_outpath = './output/input_changes.csv'
-	output_changes_outpath = './output/output_changes.csv'
+	cleaned_inputs = 'daf2-dauer-comparisons\output\input_percentages_comparisons_only.json'
+	cleaned_outputs = 'daf2-dauer-comparisons\output\output_percentages_comparions_only.json'
+	
+	#csv output paths
+	csv_input_changes_outpath = 'daf2-dauer-comparisons\output\input_changes.csv'
+	csv_output_changes_outpath = 'daf2-dauer-comparisons\output\output_changes.csv'
 
+	#json output paths
+	json_input_changes_outpath = 'daf2-dauer-comparisons\output\input_changes.json'
+	json_output_changes_outpath = 'daf2-dauer-comparisons\output\output_changes.json'
 
 	input_dict = format_data(inputs, cleaned_inputs)
 	output_dict = format_data(outputs, cleaned_outputs)
 
-	input_changes_analysis = bootstrap_sampling(input_dict)
-	analysis_results_to_csv(input_changes_outpath, input_changes_analysis)
+	input_with_median = additional_calculations(input_dict)
+	input_changes_analysis = bootstrap_sampling(input_with_median)
+	write_json(json_input_changes_outpath, input_changes_analysis)
+	#analysis_results_to_csv(csv_input_changes_outpath, input_changes_analysis)
 
-	output_changes_analysis = bootstrap_sampling(output_dict)
-	analysis_results_to_csv(output_changes_outpath, output_changes_analysis)
+	output_with_median = additional_calculations(output_dict)
+	output_changes_analysis = bootstrap_sampling(output_with_median)
+	write_json(json_output_changes_outpath, output_changes_analysis)
+	#analysis_results_to_csv(csv_output_changes_outpath, output_changes_analysis)
 	
 	
