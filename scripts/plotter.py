8d3c25b0242779bae3340fb25a02a73162da2380
MonaDXWang <45204797+MonaDXWang@users.noreply.github.com>
2021-06-23 08:32:40 -0400

    Improved plotter

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,6 +1,9 @@
 import numpy as np
+from numpy.lib.nanfunctions import _nanmedian1d
+from numpy.ma import compress
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 import plotly.express as px
@@ -31,7 +34,23 @@ def scatter_plotter(path, title, synapse_type = 'count'):
 
     fig.show()
 
-def histogram(path, title, plot = plot, synapse_type = 'count'):
+def hist_from_list(data,folder_name, title, bins, range):
+    
+    filter = np.isfinite(data)
+    plot = list(compress(filter, data))
+
+    new_plot = [(range[0]-5) if value < range[0] else (range[1]+5) if value > range[1] else value for value in plot]
+    plt.figure(figsize=(8,6))
+    plt.hist(new_plot, bins = bins)
+    
+    #comment out if wanting to see all the data
+    #plt.xlim(range[0], range[1])
+    #plt.ylim(0, 2500)
+    plt.title(title)
+
+    plt.savefig(f'./graphs/{folder_name}/{title}.pdf', bbox_inches = 'tight')
+
+def histogram_from_csv(path, title, plot = plot, synapse_type = 'count'):
 
     df = pd.read_csv(path)
 
@@ -66,22 +85,28 @@ def kernel_density_plot(path, title):
 
     fig.show()
 
-def multi_histogram(data1, data2, data3):
+def multi_histogram(data1, data2, data3, name1, name2, name3):
+
+
+
+    data12 = data1.merge(data2, left_index = True, right_index = True)
+    dataf = data12.merge(data3, left_index = True, right_index = True)
+    dataf.to_csv('./output.csv')
 
     timepoints = np.array([4.3, 16, 23, 27, 50])
     xlabel = 'Predited hours after birth'
     ylabel = 'Number of daf2 dauer connections'
     title = 'Neural age prediction based on different normalization methods'
 
-    plt.figure(figsize=(8,6))
-    plt.hist(data1, bins=20, alpha=0.5, label=data1.columns[0])
-    plt.hist(data2, bins=20, alpha=0.5, label=data2.columns[0])
-    plt.hist(data3, bins=20, alpha=0.5, label=data3.columns[0])
+    
+    #plt.figure(figsize=(8,6))
+    #sns.histplot(data = dataf[data1.columns[0]], color = 'skyblue', label=dataf.columns[0], kde = True)
+    #sns.histplot(data = dataf[dataf.columns[1]], color= 'gold', label=dataf.columns[1], kde = True)
+    #sns.histplot(data = dataf[dataf.columns[2]], color= 'teal', label=dataf.columns[2], kde = True)
 
      
     x_axis = np.arange(-250, 150, 50)
     xticks = np.concatenate((x_axis, timepoints))
-
 
     plt.title(title)
     plt.xlabel(xlabel, size=14)
